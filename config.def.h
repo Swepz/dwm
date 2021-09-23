afaa4867c7dd1b61dcb95527ccc4f577615d6f58
swepz <taza@live.se>
2021-09-23 13:47:41 +0200

    Completed

--- config.def.h
+++ config.def.h
@@ -123,6 +123,7 @@ static Key keys[] = {
 	{ MODKEY|ControlMask,           XK_period, cyclelayout,    {.i = +1 } },
 	{ MODKEY,                       XK_space,  cyclelayout,    {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+  { MODKEY,                       XK_s,      togglesticky,   {0} },
 	{ MODKEY,             XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
